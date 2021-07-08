930060a71a590649af35f7534d691c66300e03d1
Antonio DiMaggio <antoniomdimaggio@gmail.com>
2021-07-07 21:01:57 -0700

    Update gcd_lcm_coprime_tests.py

--- tests/gcd_lcm_coprime_tests.py
+++ tests/gcd_lcm_coprime_tests.py
@@ -2,10 +2,12 @@ import dimaggio.math
 
 
 def main():
-    a, b = 27, 33
-    print(dimaggio.math.gcd(a, b))
-    print(dimaggio.math.lcm(a, b))
-    print(dimaggio.math.isCoprime(a, b))
+    assert dimaggio.math.gcd(27, 33) == 3
+    assert dimaggio.math.lcm(27, 33) == 297
+    assert dimaggio.math.isCoprime(27, 33) is False
+    assert dimaggio.math.gcd(3, 5) == 1
+    assert dimaggio.math.lcm(3, 5) == 15
+    assert dimaggio.math.isCoprime(3, 5) is True
 
 
 if "__main__" == __name__:
