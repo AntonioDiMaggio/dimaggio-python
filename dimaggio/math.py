7616dd3f161a547f7da66153c0733ff80fd8f80b
Antonio DiMaggio <antoniomdimaggio@gmail.com>
2021-07-07 19:27:11 -0700

    Added test for previous commit.

--- dimaggio/math.py
+++ dimaggio/math.py
@@ -1,10 +1,12 @@
 def gcd(a: int, b: int) -> int:
-	while b:
-		a, b = b, a % b		
-	return a
+    while b:
+        a, b = b, a % b
+    return a
+
 
 def lcm(a: int, b: int) -> int:
-	return a * b // gcd(a, b)
+    return a * b // gcd(a, b)
 
-def is_coprime(a: int, b: int) -> bool:
-	return 1 == gcd(a, b)
+
+def isCoprime(a: int, b: int) -> bool:
+    return 1 == gcd(a, b)
